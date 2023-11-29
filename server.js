a09e589150d58063a2ab17363669160ff1a9bfe0
Danil Sch <danil80sch@gmail.com>
2023-11-29 15:36:34 +0100

    startup and shutdown command, with test pings

--- server.js
+++ server.js
@@ -1,14 +1,25 @@
 const express = require('express');
+const exec = require('child_process').exec;
 const bodyParser = require('body-parser');
+const http = require("http");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
-app.post('/', (req, res) => {
+app.post('/startup', (req, res) => {
+    exec("ping -c 1 google.com", (err, stdout, stderr) => {
+        console.log(stdout);
+    });
     res.sendStatus(200);
-    console.log('Received webhook:', req.body);
+});
+
+app.post('/shutdown', (req, res) => {
+    exec("ping -c 1 stadtlohn-hlr.de", (err, stdout, stderr) => {
+        console.log(stdout);
+    });
+    res.sendStatus(200);
 });
 
 app.listen(PORT, () => {
