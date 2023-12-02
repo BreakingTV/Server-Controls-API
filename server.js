e8c821ba91a8a8ddf4f3cd11fff3ec737604efe8
Danil Schumin <danil80sch@gmail.com>
2023-12-02 20:06:32 +0100

    Added startup and shutdown command

--- server.js
+++ server.js
@@ -1,30 +1,56 @@
 const express = require('express');
-const { exec} = require('child_process');
+const Client = require('ssh2').Client;
 const bodyParser = require('body-parser');
+const dotenv = require("dotenv");
+const wol = require('wol');
+
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+dotenv.config();
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.post('/startup', (req, res) => {
     res.sendStatus(200);
 
+    wol.wake('94:40:c9:ed:6c:a0', (err, res) => {
+        if (err) console.log(err);
+        else console.log(res);
+    })
 });
 
 app.post('/shutdown', (req, res) => {
     res.sendStatus(200);
-    serverStatus();
+    executeCommand('shutdown -P now');
 });
 
 app.get('/status', (req, res) => {
     res.sendStatus(200);
-
+    executeCommand('uptime');
 });
 
 
-function serverStatus() {
-    // TODO: change this to local Server, this wont work if NGINX Server is down but the Server is up
-    exec('ping -w 1 semiko.tech', console.log);
+function executeCommand(cmd) {
+    const conn = new Client;
+    conn.on('ready', () => {
+        conn.exec('sudo ' + cmd, { pty: true }, (err, stream) => {
+            if (err) throw err;
+            stream.on('close', (code, signal) => {
+                console.log('STREAM END');
+                conn.end();
+            }).on('data', (data) => {
+                if (data.indexOf(':') >= data.length - 2) stream.write(process.env.PASSWORD + '\n');
+                else console.log('OUTPUT: ' + data)
+            });
+        })
+    }).on('error', err => {
+        console.log('CONNECTION ERROR');
+    }).connect({
+        host: process.env.HOST,
+        username: process.env.USERNAME,
+        password: process.env.PASSWORD
+    });
+
 }
 app.listen(PORT, () => { console.log(`Server is running on port ${PORT}`); });
