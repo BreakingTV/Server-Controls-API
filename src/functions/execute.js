27b2af122c0c7b14010e01889d66234b7cf423f2
Danil Schumin <danil80sch@gmail.com>
2023-12-24 13:51:56 +0100

    Added Check if Server is online Need to rewrite the entire mess that is the execute.js...

--- src/functions/execute.js
+++ src/functions/execute.js
@@ -1,31 +1,52 @@
 const {Client} = require("ssh2");
+const os = require("os");
+const exec = require('child_process').exec;
 
 async function command(cmd, useSudo = true) {
-    const conn = new Client;
-    return new Promise((resolve, reject) => {
-        let res;
-        conn.on('ready', () => {
-            conn.exec((useSudo) ? 'sudo ' + cmd : cmd, {pty: true}, (err, stream) => {
-                if (err) throw err;
-                stream.on('close', (code, signal) => {
-                    resolve(res.replace('undefined', ''));
-                    conn.end();
-                }).on('data', (data) => {
-                    if (data.indexOf(':') >= data.length - 2) stream.write(process.env.PASSWORD + '\n');
-                    else {
-                            res += data;
-                    }
+    // TODO: rewrite this mess
 
+    const conn = new Client;
+        if (await checkStatus() === true) {
+            return new Promise(async (resolve, reject) => {
+                let res;
+                conn.on('ready', () => {
+                    conn.exec((useSudo) ? 'sudo ' + cmd : cmd, {pty: true}, (err, stream) => {
+                        if (err) throw err;
+                        stream.on('close', (code, signal) => {
+                            resolve(res.replace('undefined', ''));
+                            conn.end();
+                        }).on('data', (data) => {
+                            if (data.indexOf(':') >= data.length - 2) stream.write(process.env.PASSWORD + '\n');
+                            else {
+                                res += data;
+                            }
+
+                        });
+                    })
+                }).on('error', err => {
+                    console.log('CONNECTION ERROR: ' + err);
+                }).connect({
+                    host: process.env.HOST,
+                    username: process.env.USERNAME,
+                    password: process.env.PASSWORD
                 });
-            })
-        }).on('error', err => {
-            console.log('CONNECTION ERROR: ' + err);
-        }).connect({
-            host: process.env.HOST,
-            username: process.env.USERNAME,
-            password: process.env.PASSWORD
-        });
-    });
+            });
+        } else {
+            return {data: {status: "OFFLINE"}};
+        }
+}
+
+async function checkStatus() {
+    return new Promise((resolve, reject) => {
+        exec('ping -c 1 ' + ((process.env.IPV6 === undefined) ? process.env.HOST : process.env.IPV6), (error, stdout) => {
+            if (error) {
+                resolve(false);
+                return;
+            }
+            resolve(true)
+        })
+    })
+
 }
 
 module.exports = { command }
--- src/router/status.js
+++ src/router/status.js
@@ -9,10 +9,14 @@ const timers = require("timers");
 
 router.get('/uptime', async (req, res) => {
     await execute.command('uptime').then(result => {
-        let uptime = result.toString().split('up ')[1].split(',')[0];
-        let average = result.toString().split('load average: ')[1].replace(/(\r\n|\n|\r)/gm, "");
-        res.json({data: {time: uptime, loadAverage: average}});
+        if (typeof result === 'string') {
+            let uptime = result.toString().split('up ')[1].split(',')[0];
+            let average = result.toString().split('load average: ')[1].replace(/(\r\n|\n|\r)/gm, "");
+            res.json({data: {time: uptime, loadAverage: average}});
+        } else res.json(result);
     }).catch(err => console.log(err));
 });
+
+
 
 module.exports = router;
